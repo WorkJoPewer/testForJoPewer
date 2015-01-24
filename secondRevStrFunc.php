bc2b3d7c85d7d9c2cbec6bf8005c2f2834f093aa
WorkJoPewer <workjopewer@gmail.com>
2015-01-24 13:07:28 +0200

    revStrFuncRightLoop

--- secondRevStrFunc.php
+++ secondRevStrFunc.php
@@ -2,7 +2,9 @@
 function revStr($str="hello")
 {
 $out = '';
-for ($i = strlen($str) - 1; $i >= 0; $out .= $str[$i--]) {}
+$len= strlen($str);
+for ($i=$len -1; $i>=0; $i--)
+$out .=$str[$i];
 return $out;
 }
 echo revStr();
